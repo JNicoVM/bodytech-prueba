7b62738bbe848ff1f3f3da49df1c7d2a776b8538
JNicoVM <jnvmud@gmail.com>
2021-03-02 01:16:59 -0500

    Terminando detalles en logica

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -25,27 +25,29 @@ export class RegisterPage implements OnInit {
   }
 
   onRegister(){
-    // console.log(this.formBuilder.group({
-    //   email: ['', Validators.compose([Validators.maxLength(70), Validators.pattern('^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$'), Validators.required])]
-    // })); 
-    this.authService.register(this.email, this.password).then(res => {
-      alert("Usuario creado con exito!");
-      this.authService.logout;
-      this.router.navigate(["/login"]);
-    }).catch(
-      err => {
-        if(err["code"]=="auth/invalid-email"){
-          alert("Ese correo electrónico no es aceptado!");
-        }else if (err["code"]=="auth/email-already-in-use")
-        {
-          alert("Este correo electrónico ya esta registrado!");
+    if(this.password != this.passwordConfirm){
+      alert("Las contraseñas no coinciden");
+    }else{
+      this.authService.register(this.email, this.password).then(res => {
+        alert("Usuario creado con exito!");
+        this.authService.logout;
+        this.router.navigate(["/login"]);
+      }).catch(
+        err => {
+          if(err["code"]=="auth/invalid-email"){
+            alert("Ese correo electrónico no es aceptado!");
+          }else if (err["code"]=="auth/email-already-in-use")
+          {
+            alert("Este correo electrónico ya esta registrado!");
+          }
+          else if (err["code"]=="auth/weak-password")
+          {
+            alert("La contraseña debe tener al menos 6 caracteres!");
+          }
+          
         }
-        else if (err["code"]=="auth/weak-password")
-        {
-          alert("La contraseña debe tener al menos 6 caracteres!");
-        }
-        
-      }
-    )
+      )
+    }
+    
   }
 }
